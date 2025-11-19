2b9967145fd2f2d1e7ddf2d105b31814d87336e1
Matheus Marques <matheusmarquesoliv@gmail.com>
2025-11-18 22:56:13 -0300

    Initial commit

---
+++ src/main/java/com/matheusmarques/nfgeneration/NfgenerationApplication.java
@@ -0,0 +1,13 @@
+package com.matheusmarques.nfgeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfgenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfgenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusmarques/nfgeneration/NfgenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusmarques.nfgeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfgenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
